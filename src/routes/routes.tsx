dd58de63224f0d527522a15f0ae58df7c28afa9c
Jhonata Carvalho <jhonata.carvalho@tascomtecnologia.com.br>
2024-02-21 22:09:29 -0300

    add: initial screen and routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,16 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home } from "../pages/Home";
+import { Play } from "../pages/Play";
+
+export function Routes() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Play} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
